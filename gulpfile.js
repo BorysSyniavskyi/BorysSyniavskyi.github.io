ff6f4ef9f25749bfce591645a21badd0ca375fd9
Borys <irvine18061993@gmail.com>
2022-03-19 20:10:51 +0200

    levelup gulp and fix wordpress

--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,11 @@ gulp.task('server', function() {
 
     browserSync({
         server: {
-            baseDir: "src"
+            baseDir: "./"
         }
     });
 
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("**/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
@@ -26,8 +26,38 @@ gulp.task('styles', function() {
         .pipe(browserSync.stream());
 });
 
+gulp.task('styles', function() {
+    return gulp.src("src/pulse/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/pulse/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/uber/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/uber/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/wordpress/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/wordpress/css"))
+        .pipe(browserSync.stream());
+});
+
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    gulp.watch("**/*.+(scss|sass)", gulp.parallel('styles'));
 })
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
